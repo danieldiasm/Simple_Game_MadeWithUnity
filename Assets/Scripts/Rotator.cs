ed8032f682040b4b2d7437ee71f9e1ed71e4cec0
Daniel Dias de Moraes <daniel_diasm@hotmail.com>
2016-09-26 23:57:29 -0300

    Just uploading
    No big deal...

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () 
+	{
+		transform.Rotate (new Vector3 (25,25,25) * Time.deltaTime);
+	}
+}
